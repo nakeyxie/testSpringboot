5d46b99a2e1198d83cbcbbf076349994afbd4e4a
nakeyxie <pang19910306>
2021-04-13 18:05:36 +0800

    补充代码

--- src/test/java/com/xiechy/test/run1.java
+++ src/test/java/com/xiechy/test/run1.java
@@ -1,5 +1,12 @@
 package com.xiechy.test;
 
+/**
+ * @Description Runnable实现
+ * @Author xcy
+ * @Date  2021/4/13 17:39
+ * @param null
+ * @return
+*/
 public class run1 implements Runnable {
 
 	@Override
